fae6853442dada8c3b02ff280a36be5f435b77ec
WIN-NSLDP27H68E\User478734 <brilook@gmail.com>
2021-10-05 16:34:48 +0300

    "logged in" status

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import {Route} from "react-router-dom";
 import Settings from "./components/Settings/Settings";
@@ -11,12 +10,13 @@ import Photos from "./components/Photos/Photos";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
 
   return (
     <div className="app-wrapper">
-      <Header/>
+      <HeaderContainer/>
       <Navbar/>
       <div className="app-wrapper-content">
         <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './Header.module.css';
 import '../vireables.css';
+import login from '../images/login.svg'
 
 //
 // import logoBlack from '../images/Logo-Black-wine.png';
 // import logoWhite from '../images/Logo-White.png';
 import logoWhiteSvg from '../images/Logo-White.svg';
+import {NavLink} from "react-router-dom";
 
 
-const Header = () => {
+const Header = (props) => {
 
     return (
         <header className={`bg ${styles.header}`}>
             <img  src={logoWhiteSvg} alt="logo"></img>
+          <div className={styles.loginBlock}>
+            {props.isAuth ? props.login
+              : <NavLink to={'/login'}><img className={styles.loginIcon}  src={login} alt="login"></img></NavLink>}
+          </div>
         </header>
     )
 };
